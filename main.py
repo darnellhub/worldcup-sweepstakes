96f3a949cf669914d32baf7ff730be835a67a359
darnellhub <94544545+darnellhub@users.noreply.github.com>
2022-11-14 21:30:58 +0000

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def get_coor(x, y):
     pyperclip.copy(str(x) + " " + str(y))
     print(x, y)
 
-user_mode = screen.textinput(title="World Cup Sweepstakes",
+# user_mode = screen.textinput(title="World Cup Sweepstakes",
                                  prompt="Are you ready to join the World Cup Fantasy League?")
 
 user_name = screen.textinput(title="World Cup Sweepstakes",
